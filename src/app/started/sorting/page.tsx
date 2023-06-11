58232c82bf9e0668bd1dc60a603d4c71e8db671e
JonatasMSS <jonatas.miguelss@gmail.com>
2023-06-11 12:03:32 -0300

    [fix]: cardsSelected will be one when are in initial state

--- src/app/started/sorting/page.tsx
+++ src/app/started/sorting/page.tsx
@@ -10,7 +10,7 @@ export default function Sorting() {
   const [cardsSelected, setCardsSelected] = useState(0)
   const [activePopUp, setPopUp] = useState(false)
 
-  const [maxCards, setMaxCards] = useState(0)
+  const [maxCards, setMaxCards] = useState(1) // Começar com um, pois demora a atualizar
   const [cards, setCards] = useState<Card[]>([])
 
   useEffect(() => {
@@ -24,7 +24,7 @@ export default function Sorting() {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cardsSelected])
-
+  console.log(cardsSelected)
   return (
     <div className=" relative flex w-full flex-col items-center justify-center bg-[#1C1C1C]">
       {activePopUp && (
